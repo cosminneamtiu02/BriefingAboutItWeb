d32159ac2ca04f0b1d49c3b817bc0c8b139eb2c0
cocojambo02 <cosmin.neamtiu@stud.ubbcluj.ro>
2023-06-10 16:13:28 +0300

    few structural changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    login: './src/index.js',
+    home: './src/home.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   mode: 'development',
   devServer: {
@@ -19,6 +34,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      filename: 'index.html',
+      chunks: ['login'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/home.html',
+      filename: 'home.html',
+      chunks: ['home'],
     }),
   ],
+  
 };
